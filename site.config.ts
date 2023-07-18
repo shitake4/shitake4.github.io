46cb040dc1cfe4e264c7c6ff9f2fa51dd0d410d5
shitake4 <dev.t.suzuki@gmail.com>
2023-07-18 12:25:07 +0900

    デプロイ設定とog画像の変更

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export const config = {
   },
   siteRoot:
     process.env.NODE_ENV === "production"
-      ? "https://team-blog-hub.vercel.app"
+      ? "https://shitake4.tech"
       : "http://localhost:3000",
   headerLinks: [
     {
