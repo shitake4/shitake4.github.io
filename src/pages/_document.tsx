38ef9a6347265de1d6b737c9925bf4ab62267d6b
shitake4 <dev.t.suzuki@gmail.com>
2026-01-08 20:13:35 +0900

    :recycle: refactor: 関数型コンポーネントへ置き換え

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,29 +1,24 @@
-import Document, {Head, Html, Main, NextScript} from "next/document";
+import {Head, Html, Main, NextScript} from "next/document";
 import {config} from "@site.config";
-import Link from "next/link";
 import {inter} from "@src/lib/fonts";
 
-class MyDocument extends Document {
-  render() {
-    return (
-        <Html className={inter.className} lang="ja">
-          <Head>
-            <meta charSet="utf-8"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <meta name="theme-color" content="#000000"/>
-            <Link
-                rel="icon shortcut"
-                type="image/png"
-                href={`${config.siteRoot}/logo.png`}
-            />
-          </Head>
-          <body>
-          <Main/>
-          <NextScript/>
-          </body>
-        </Html>
-    );
-  }
+export default function Document() {
+  return (
+      <Html className={inter.className} lang="ja">
+        <Head>
+          <meta charSet="utf-8"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+          <meta name="theme-color" content="#000000"/>
+          <link
+              rel="icon shortcut"
+              type="image/png"
+              href={`${config.siteRoot}/logo.png`}
+          />
+        </Head>
+        <body>
+        <Main/>
+        <NextScript/>
+        </body>
+      </Html>
+  );
 }
-
-export default MyDocument;
